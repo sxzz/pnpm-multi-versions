a14dcc1a745bffb07d9651ed4b826dfca36cdb93
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-01-31 20:35:59 +0800

    feat(cli): custom lockfile & ignore major

--- src/cli.ts
+++ src/cli.ts
@@ -1,16 +1,33 @@
 import process from 'node:process'
+import { cac } from 'cac'
 import { findUp } from 'find-up-simple'
 import pc from 'picocolors'
+import { version } from '../package.json'
 import { pnpmMultiVersions, readLockfile } from './index'
 
-export async function runCLI(): Promise<void> {
-  const filePath = await findUp('pnpm-lock.yaml')
+export function runCLI(): void {
+  const cli = cac('pnpm-multi-versions')
+  cli.help().version(version)
+  cli
+    .option('--ignore-major', 'Ignore major version difference')
+    .command('[lockfile]', 'Find multiple versions packages in pnpm-lock.yaml')
+    .action(run)
+  cli.parse()
+}
+
+async function run(
+  file: string | undefined,
+  { ignoreMajor }: { ignoreMajor?: boolean } = {},
+) {
+  const filePath = file || (await findUp('pnpm-lock.yaml'))
   if (!filePath) {
     console.error(pc.red('pnpm-lock.yaml not found!'))
     process.exit(1)
   }
   const lockfile = await readLockfile(filePath)
-  const { versionsMap, multipleVersions } = pnpmMultiVersions(lockfile)
+  const { versionsMap, multipleVersions } = pnpmMultiVersions(lockfile, {
+    ignoreMajor: !!ignoreMajor,
+  })
 
   if (multipleVersions.length === 0) {
     console.log(pc.green('No multiple versions packages found!'))
